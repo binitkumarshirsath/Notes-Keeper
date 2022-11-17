2a5f800719c520b7604b0187ebcf560949f7c699
unknown <binitkumar0911@gmail.com>
2022-11-17 22:25:09 +0530

    Initial commit add css and create folder structure

---
+++ src/components/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+import Notes from "./Notes";
+
+const App = ()=>{
+     return <>
+     <Header/>
+     <Notes/>
+     <Footer/>
+     </>        
+}
+
+export default App;
