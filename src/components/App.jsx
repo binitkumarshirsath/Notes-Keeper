92967ce2cebd89d65337a9dcf118ef321e1fff1b
unknown <binitkumar0911@gmail.com>
2022-11-17 23:42:10 +0530

    Add createarea.jsx and change css

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,16 @@
 import React from "react";
 import Header from "./Header";
 import Footer from "./Footer";
-import Notes from "./Notes";
+import Notes from "./Notes.jsx";
+import CreateArea from "./CreateArea";
+
+
 
 const App = ()=>{
      return <>
      <Header/>
-     <Notes/>
+     <CreateArea/>
+     <Notes key={1} title="Note title" content="Note content" />
      <Footer/>
      </>        
 }
